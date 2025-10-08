d0e5c8bb1fbf793b88ee2bf578098eacd96251f7
Rashidul Islam <setu017100@gmail.com>
2025-10-08 19:18:39 +0600

    home1 working

---
+++ assets/js/custom.js
@@ -0,0 +1,619 @@
+(function ($) {
+  ("use strict");
+
+  $(".sidebar-button").on("click", function () {
+    $(this).toggleClass("active");
+  });
+
+  const sidebarButton = document.querySelector(".sidebar-button");
+
+  if (sidebarButton) {
+    sidebarButton.addEventListener("click", () => {
+      document.querySelector(".main-menu").classList.toggle("show-menu");
+    });
+  }
+
+  $(".menu-close-btn").on("click", function () {
+    $(".main-menu").removeClass("show-menu");
+  });
+
+  // sidebar
+  $(".right-sidebar-button").on("click", function () {
+    $(".right-sidebar-menu").addClass("show-right-menu");
+  });
+  $(".right-sidebar-close-btn").on("click", function () {
+    $(".right-sidebar-menu").removeClass("show-right-menu");
+  });
+
+  $(".menu-btn").on("click", function () {
+    $(".sidebar-menu").addClass("active");
+  });
+  $(".sidebar-menu-close").on("click", function () {
+    $(".sidebar-menu").removeClass("active");
+  });
+
+  jQuery(".dropdown-icon").on("click", function () {
+    jQuery(this)
+      .toggleClass("active")
+      .next("ul, .mega-menu")
+      .slideToggle();
+    jQuery(this)
+      .parent()
+      .siblings()
+      .children("ul, .mega-menu")
+      .slideUp();
+    jQuery(this).parent().siblings().children(".active").removeClass("active");
+  });
+  jQuery(".dropdown-icon2").on("click", function () {
+    jQuery(this).toggleClass("active").next(".submenu-list").slideToggle();
+    jQuery(this).parent().siblings().children(".submenu-list").slideUp();
+    jQuery(this).parent().siblings().children(".active").removeClass("active");
+  });
+
+
+  // sticky header
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('header.header-area');
+    if (header) {
+      header.classList.toggle("sticky", window.scrollY > 0);
+    }
+  });
+
+  // FancyBox Js
+  $('[data-fancybox="gallery-01"]').fancybox({
+    buttons: ["close"],
+    loop: false,
+    protect: true,
+  });
+  $('[data-fancybox="video-player"]').fancybox({
+    buttons: ["close"],
+    loop: false,
+    protect: true,
+  });
+
+  //Counter up
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+  // Blog Card Slider
+  var swiper = new Swiper(".blog-card-slider", {
+    slidesPerView: 1,
+    speed: 1500,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+      delay: 2500, // Autoplay duration in milliseconds
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".blog-slider-next",
+      prevEl: ".blog-slider-prev",
+    },
+    pagination: {
+      el: ".franctional-pagi1",
+      type: "fraction",
+    },
+    breakpoints: {
+      280: {
+        slidesPerView: 1,
+      },
+      386: {
+        slidesPerView: 1,
+      },
+      576: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  // Home1 Banner Slider
+  var swiper = new Swiper(".home1-banner-slider", {
+    slidesPerView: 1,
+    speed: 1500,
+    spaceBetween: 24,
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: {
+      nextEl: ".banner-slider-next",
+      prevEl: ".banner-slider-prev",
+    },
+    pagination: {
+      el: ".banner-pagination",
+      clickable: true,
+    },
+  });
+  //wow js
+  jQuery(window).on("load", function () {
+    new WOW().init();
+    window.wow = new WOW({
+      boxClass: "wow",
+      animateClass: "animated",
+      offset: 0,
+      mobile: true,
+      live: true,
+      offset: 80,
+    });
+    window.wow.init();
+  });
+
+  // niceSelect
+  if ($("select").length) {
+    $("select").niceSelect();
+  }
+
+  //Quantity Increment
+  $(".quantity__minus").on("click", function (e) {
+    e.preventDefault();
+    var input = $(this).siblings(".quantity__input");
+    var value = parseInt(input.val(), 10);
+    if (value > 1) {
+      value--;
+    }
+    input.val(value.toString().padStart(2, "0"));
+  });
+  $(".quantity__plus").on("click", function (e) {
+    e.preventDefault();
+    var input = $(this).siblings(".quantity__input");
+    var value = parseInt(input.val(), 10);
+    value++;
+    input.val(value.toString().padStart(2, "0"));
+  });
+
+  if ($("body").not(".is-mobile").hasClass("tt-magic-cursor")) {
+    if ($(window).width() > 1024) {
+      gsap.config({
+        nullTargetWarn: false,
+        trialWarn: false,
+      });
+      $(".magnetic-item").wrap('<div class="magnetic-wrap"></div>');
+
+      if ($("a.magnetic-item").length) {
+        $("a.magnetic-item").addClass("not-hide-cursor");
+      }
+
+      var $mouse = { x: 0, y: 0 }; // Cursor position
+      var $pos = { x: 0, y: 0 }; // Cursor position
+      var $ratio = 0.15; // delay follow cursor
+      var $active = false;
+      var $ball = $("#ball");
+
+      var $ballWidth = 20; // Ball default width
+      var $ballHeight = 20; // Ball default height
+      var $ballOpacity = 0.5; // Ball default opacity
+      var $ballBorderWidth = 2; // Ball default border width
+
+      gsap.set($ball, {
+        // scale from middle and style ball
+        xPercent: -50,
+        yPercent: -50,
+        width: $ballWidth,
+        height: $ballHeight,
+        borderWidth: $ballBorderWidth,
+        opacity: $ballOpacity,
+      });
+
+      document.addEventListener("mousemove", mouseMove);
+
+      function mouseMove(e) {
+        $mouse.x = e.clientX;
+        $mouse.y = e.clientY;
+      }
+
+      gsap.ticker.add(updatePosition);
+
+      function updatePosition() {
+        if (!$active) {
+          $pos.x += ($mouse.x - $pos.x) * $ratio;
+          $pos.y += ($mouse.y - $pos.y) * $ratio;
+
+          gsap.set($ball, { x: $pos.x, y: $pos.y });
+        }
+      }
+
+      $(".magnetic-wrap").mousemove(function (e) {
+        parallaxCursor(e, this, 2); // magnetic ball = low number is more attractive
+        callParallax(e, this);
+      });
+
+      function callParallax(e, parent) {
+        parallaxIt(e, parent, parent.querySelector(".magnetic-item"), 25); // magnetic area = higher number is more attractive
+      }
+
+      function parallaxIt(e, parent, target, movement) {
+        var boundingRect = parent.getBoundingClientRect();
+        var relX = e.clientX - boundingRect.left;
+        var relY = e.clientY - boundingRect.top;
+
+        gsap.to(target, {
+          duration: 0.3,
+          x: ((relX - boundingRect.width / 2) / boundingRect.width) * movement,
+          y:
+            ((relY - boundingRect.height / 2) / boundingRect.height) * movement,
+          ease: Power2.easeOut,
+        });
+      }
+
+      function parallaxCursor(e, parent, movement) {
+        var rect = parent.getBoundingClientRect();
+        var relX = e.clientX - rect.left;
+        var relY = e.clientY - rect.top;
+        $pos.x =
+          rect.left + rect.width / 2 + (relX - rect.width / 2) / movement;
+        $pos.y =
+          rect.top + rect.height / 2 + (relY - rect.height / 2) / movement;
+        gsap.to($ball, { duration: 0.3, x: $pos.x, y: $pos.y });
+      }
+
+      // Magic cursor behavior
+      // ======================
+
+      // Magnetic item hover.
+      $(".magnetic-wrap")
+        .on("mouseenter mouseover", function (e) {
+          $ball.addClass("magnetic-active");
+          gsap.to($ball, { duration: 0.3, width: 70, height: 70, opacity: 1 });
+          $active = true;
+        })
+        .on("mouseleave", function (e) {
+          $ball.removeClass("magnetic-active");
+          gsap.to($ball, {
+            duration: 0.3,
+            width: $ballWidth,
+            height: $ballHeight,
+            opacity: $ballOpacity,
+          });
+          gsap.to(this.querySelector(".magnetic-item"), {
+            duration: 0.3,
+            x: 0,
+            y: 0,
+            clearProps: "all",
+          });
+          $active = false;
+        });
+
+      // Alternative cursor style on hover.
+      $(
+        ".cursor-alter, .tt-main-menu-list > li > a, .tt-main-menu-list > li > .tt-submenu-trigger > a"
+      )
+        .not(".magnetic-item") // omit from selection.
+        .on("mouseenter", function () {
+          gsap.to($ball, {
+            duration: 0.3,
+            borderWidth: 0,
+            opacity: 0.2,
+            backgroundColor: "#CCC",
+            width: "90px",
+            height: "90px",
+          });
+        })
+        .on("mouseleave", function () {
+          gsap.to($ball, {
+            duration: 0.3,
+            borderWidth: $ballBorderWidth,
+            opacity: $ballOpacity,
+            backgroundColor: "transparent",
+            width: $ballWidth,
+            height: $ballHeight,
+            clearProps: "backgroundColor",
+          });
+        });
+
+      // Cursor view on hover (data attribute "data-cursor="...").
+      $("[data-cursor]").each(function () {
+        $(this)
+          .on("mouseenter", function () {
+            $ball
+              .addClass("ball-view")
+              .append('<div class="ball-view-inner"></div>');
+            $(".ball-view-inner").append($(this).attr("data-cursor"));
+            gsap.to($ball, {
+              duration: 0.3,
+              yPercent: -75,
+              width: 82,
+              height: 82,
+              opacity: 1,
+              borderWidth: 0,
+            });
+            gsap.to(".ball-view-inner", {
+              duration: 0.3,
+              scale: 1,
+              autoAlpha: 1,
+            });
+          })
+          .on("mouseleave", function () {
+            gsap.to($ball, {
+              duration: 0.3,
+              yPercent: -50,
+              width: $ballWidth,
+              height: $ballHeight,
+              opacity: $ballOpacity,
+              borderWidth: $ballBorderWidth,
+            });
+            $ball.removeClass("ball-view").find(".ball-view-inner").remove();
+          });
+        $(this).addClass("not-hide-cursor");
+      });
+
+      // Cursor drag on hover (class "cursor-drag"). For Swiper sliders.
+      $(".swiper").each(function () {
+        if ($(this).parent().attr("data-simulate-touch") === "true") {
+          if ($(this).parent().hasClass("cursor-drag")) {
+            $(this)
+              .on("mouseenter", function () {
+                $ball.append('<div class="ball-drag"></div>');
+                gsap.to($ball, {
+                  duration: 0.3,
+                  width: 60,
+                  height: 60,
+                  opacity: 1,
+                });
+              })
+              .on("mouseleave", function () {
+                $ball.find(".ball-drag").remove();
+                gsap.to($ball, {
+                  duration: 0.3,
+                  width: $ballWidth,
+                  height: $ballHeight,
+                  opacity: $ballOpacity,
+                });
+              });
+            $(this).addClass("not-hide-cursor");
+
+            // Ignore "data-cursor" on hover.
+            $(this)
+              .find("[data-cursor]")
+              .on("mouseenter mouseover", function () {
+                $ball.find(".ball-drag").remove();
+                return false;
+              })
+              .on("mouseleave", function () {
+                $ball.append('<div class="ball-drag"></div>');
+                gsap.to($ball, {
+                  duration: 0.3,
+                  width: 60,
+                  height: 60,
+                  opacity: 1,
+                });
+              });
+          }
+        }
+      });
+
+      // Cursor drag on mouse down / click and hold effect (class "cursor-drag-mouse-down"). For Swiper sliders.
+      $(".swiper").each(function () {
+        if ($(this).parent().attr("data-simulate-touch") === "true") {
+          if ($(this).parent().hasClass("cursor-drag-mouse-down")) {
+            $(this)
+              .on("mousedown pointerdown", function (e) {
+                if (e.which === 1) {
+                  // Affects the left mouse button only!
+                  gsap.to($ball, {
+                    duration: 0.2,
+                    width: 60,
+                    height: 60,
+                    opacity: 1,
+                  });
+                  $ball.append('<div class="ball-drag"></div>');
+                }
+              })
+              .on("mouseup pointerup", function () {
+                $ball.find(".ball-drag").remove();
+                if ($(this).find("[data-cursor]:hover").length) {
+                } else {
+                  gsap.to($ball, {
+                    duration: 0.2,
+                    width: $ballWidth,
+                    height: $ballHeight,
+                    opacity: $ballOpacity,
+                  });
+                }
+              })
+              .on("mouseleave", function () {
+                $ball.find(".ball-drag").remove();
+                gsap.to($ball, {
+                  duration: 0.2,
+                  width: $ballWidth,
+                  height: $ballHeight,
+                  opacity: $ballOpacity,
+                });
+              });
+
+            // Ignore "data-cursor" on mousedown.
+            $(this)
+              .find("[data-cursor]")
+              .on("mousedown pointerdown", function () {
+                return false;
+              });
+
+            // Ignore "data-cursor" on hover.
+            $(this)
+              .find("[data-cursor]")
+              .on("mouseenter mouseover", function () {
+                $ball.find(".ball-drag").remove();
+                return false;
+              });
+          }
+        }
+      });
+
+      // Cursor close on hover.
+      $(".cursor-close").each(function () {
+        $(this).addClass("ball-close-enabled");
+        $(this)
+          .on("mouseenter", function () {
+            $ball.addClass("ball-close-enabled");
+            $ball.append('<div class="ball-close">Close</div>');
+            gsap.to($ball, {
+              duration: 0.3,
+              yPercent: -75,
+              width: 80,
+              height: 80,
+              opacity: 1,
+            });
+            gsap.from(".ball-close", { duration: 0.3, scale: 0, autoAlpha: 0 });
+          })
+          .on("mouseleave click", function () {
+            $ball.removeClass("ball-close-enabled");
+            gsap.to($ball, {
+              duration: 0.3,
+              yPercent: -50,
+              width: $ballWidth,
+              height: $ballHeight,
+              opacity: $ballOpacity,
+            });
+            $ball.find(".ball-close").remove();
+          });
+
+        // Hover on "cursor-close" inner elements.
+        $(
+          ".cursor-close a, .cursor-close button, .cursor-close .tt-btn, .cursor-close .hide-cursor"
+        )
+          .not(".not-hide-cursor") // omit from selection (class "not-hide-cursor" is for global use).
+          .on("mouseenter", function () {
+            $ball.removeClass("ball-close-enabled");
+          })
+          .on("mouseleave", function () {
+            $ball.addClass("ball-close-enabled");
+          });
+      });
+
+      // ================================================================
+      // Scroll between anchors
+      // ================================================================
+
+      $('a[href^="#"]')
+        .not('[href$="#"]') // omit from selection
+        .not('[href$="#0"]') // omit from selection
+        .on("click", function () {
+          var target = this.hash;
+
+          // If fixed header position enabled.
+          if ($("#tt-header").hasClass("tt-header-fixed")) {
+            var $offset = $("#tt-header").height();
+          } else {
+            var $offset = 0;
+          }
+
+          // You can use data attribute (for example: data-offset="100") to set top offset in HTML markup if needed.
+          if ($(this).data("offset") != undefined)
+            $offset = $(this).data("offset");
+
+          return false;
+        });
+
+      // Show/hide magic cursor
+      // =======================
+
+      // Hide on hover.
+      $(
+        "a, button, .tt-btn, .tt-form-control, .tt-form-radio, .tt-form-check, .hide-cursor"
+      ) // class "hide-cursor" is for global use.
+        .not(".not-hide-cursor") // omit from selection (class "not-hide-cursor" is for global use).
+        .not(".cursor-alter") // omit from selection
+        .not(".tt-main-menu-list > li > a") // omit from selection
+        .not(".tt-main-menu-list > li > .tt-submenu-trigger > a") // omit from selection
+        .on("mouseenter", function () {
+          gsap.to($ball, { duration: 0.3, scale: 0, opacity: 0 });
+        })
+        .on("mouseleave", function () {
+          gsap.to($ball, { duration: 0.3, scale: 1, opacity: $ballOpacity });
+        });
+
+      // Hide on click.
+      $("a")
+        .not('[target="_blank"]') // omit from selection.
+        .not('[href^="#"]') // omit from selection.
+        .not('[href^="mailto"]') // omit from selection.
+        .not('[href^="tel"]') // omit from selection.
+        .not(".lg-trigger") // omit from selection.
+        .not(".video-player") // omit from selection.
+        .not(".tt-btn-disabled") // omit from selection.
+        .on("click", function () {
+          gsap.to($ball, { duration: 0.3, scale: 1.3, autoAlpha: 0 });
+        });
+
+      // Show/hide on document leave/enter.
+      $(document)
+        .on("mouseleave", function () {
+          gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 0 });
+        })
+        .on("mouseenter", function () {
+          gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 1 });
+        });
+
+      // Show as the mouse moves.
+      $(document).mousemove(function () {
+        gsap.to("#magic-cursor", { duration: 0.3, autoAlpha: 1 });
+      });
+    }
+  }
+
+  // Back To Top
+
+  var progressPath = document.querySelector(
+    ".progress-wrap .progress-circle path"
+  );
+  if (progressPath) {
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
+
+  // Fast Counter
+  $('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+
+})(jQuery);
