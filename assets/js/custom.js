2f73506cd656b718a78aa8b301c31d7d8dc4a2d3
Rashidul Islam <setu017100@gmail.com>
2025-10-30 15:47:43 +0600

    innerpage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1005,4 +1005,18 @@
       })
   });
 
+	// Round Text Animation
+	const element = document.querySelectorAll(".badge__char");
+	const step = 360/element.length;
+
+	element.forEach((elem, i) => {
+	elem.style.setProperty('--char-rotate', (i * step) + 'deg');
+	})
+
+	const foo = (360 / 7);
+
+	for (let i = 0; i <= 7; i++) {
+	console.log((i * foo) + 'deg');
+	}
+
 })(jQuery);
